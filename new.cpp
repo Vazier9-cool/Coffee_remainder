ba0058590ca6b399b4a6d3ad9416511f914366fd
Puneet Chougule <vaziervazier@gmail.com>
2025-05-25 20:10:55 +0530

    something initial testing

---
+++ new.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "This project uses the C++14 language standard version." << std::endl;
+    
+    return 0;
+}
